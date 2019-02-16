03aa019d78582fe21b5a3f34a020825b7be3d9d0
JustinSHong <jhong2009@gmail.com>
2019-02-15 23:39:16 -0500

    finished tests for Home component

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
+import Router from "next/router";
 
 import Home from "../pages/index.js";
 
+const mockedRouter = { push: () => {}, prefetch: () => {} };
+Router.router = mockedRouter;
+
 describe("with snapshot testing", () => {
     it("Home matches previous snapshot", () => {
-        const component = renderer.create(<App />);
+        const component = renderer.create(<Home />);
         const tree = component.toJSON();
-        console.log(tree);
         expect(tree).toMatchSnapshot();
+    });
+});
+
+describe("Home", () => {
+    it("Should have a header", () => {
+        const component = shallow(<Home />);
+        expect(component.find("h1")).toHaveLength(1);
     });
 });
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
+import React from "react";
+import Navigation from "../components/Navigation";
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
-
     }
 
     render() {
@@ -14,8 +13,7 @@ class Home extends React.Component {
                 <h1>My First Little Next.js App...featuring Star Wars!</h1>
             </div>
         );
-
     }
-};
+}
 
 export default Home;
