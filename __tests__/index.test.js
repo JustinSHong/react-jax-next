f5875b5d8ee4886ef23c18330354a3f757a7703e
JustinSHong <jhong2009@gmail.com>
2019-02-16 17:21:50 -0500

    tested for header in Home using create() from react-test-renderer

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import renderer from "react-test-renderer";
+import { create } from "react-test-renderer";
 import Router from "next/router";
 
 import Home from "../pages/index.js";
@@ -10,15 +10,29 @@ Router.router = mockedRouter;
 
 describe("with snapshot testing", () => {
     it("renders correctly", () => {
-        const component = renderer.create(<Home />);
+        const component = create(<Home />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
     });
 });
 
-describe("Home", () => {
-    it("Should have a header", () => {
+// using enzyme shallow method
+describe("Home using shallow()", () => {
+    it("should have a header", () => {
         const component = shallow(<Home />);
         expect(component.find("h1")).toHaveLength(1);
+    });
+});
+
+// using react-test-renderer
+describe("Home using create()", () => {
+    it("should have a header", () => {
+        const component = create(<Home />);
+        const header = component.root.findByType("h1");
+        console.log(header.props.children);
+        expect(header).toBeTruthy();
+        expect(header.props.children).toBe(
+            "My First Little Next.js App...featuring Star Wars!"
+        );
     });
 });
