31b8d9ea4b24f0e497c2aee42dd992feb71f9017
JustinSHong <jhong2009@gmail.com>
2019-02-20 21:45:19 -0500

    Planets: added test to check for nav bar

--- __tests__/planets.test.js
+++ __tests__/planets.test.js
@@ -3,8 +3,9 @@ import { shallow } from "enzyme";
 import { create } from "react-test-renderer";
 import Router from "next/router";
 // Components
-import Planet from "../pages/planets";
+import Planets from "../pages/planets";
 import List from "../components/List";
+import Navigation from "../components/Navigation";
 
 const mockedRouter = { push: () => {}, prefetch: () => {} };
 Router.router = mockedRouter;
@@ -13,8 +14,29 @@ const data = { results: [] };
 
 describe("with snapshot", () => {
     it("renders correctly", () => {
-        const component = create(<Planet data={data} />);
+        const component = create(<Planets data={data} />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
+    });
+});
+
+// using enzyme shallow method
+describe("Planets using shallow()", () => {
+    const component = shallow(<Planets data={data} />);
+
+    it("should have a nav bar", () => {
+        expect(component.contains(<Navigation />)).toBe(true);
+    });
+});
+
+// using react-test-renderer
+describe("Planets using shallow()", () => {
+    const component = create(<Planets data={data} />);
+
+    it("should have a nav bar", () => {
+        const nav = component.root.findByType("nav");
+
+        expect(nav).toBeTruthy();
+        expect(nav.type).toBe("nav");
     });
 });
